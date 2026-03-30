81c2603f08fcc2840e39feadab7636009e806a54
aregmk6 <areg631@gmail.com>
2026-03-30 16:14:45 +0300

    commit before new branch

--- main.cpp
+++ main.cpp
@@ -98,15 +98,6 @@ std::vector<GLuint> get_index_data() {
     return vertcies;
 }
 
-static const std::vector<glm::vec3> model_pos = {glm::vec3(-1.0f, 0.0f, -1.0f),
-                                                 glm::vec3(0.0f, 0.0f, -3.0f),
-                                                 glm::vec3(1.0f, 1.0f, -0.5f),
-                                                 glm::vec3(4.0f, 2.0f, -9.0f),
-                                                 glm::vec3(8.0f, 3.0f, -1.5f),
-                                                 glm::vec3(2.0f, -4.0f, -0.5f),
-                                                 glm::vec3(3.0f, 6.0f, -0.5f),
-                                                 glm::vec3(-5.0f, 10.0f, 3.0f)};
-
 int main() {
     ERR_CHECK(glfwInit(), "GLFW Init");
 
